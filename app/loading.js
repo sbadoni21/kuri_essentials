606202b30d1632f81f95becfc6f36a05c2cad8c5
sbadoni21 <97601162+sbadoni21@users.noreply.github.com>
2024-10-07 18:08:29 +0530

    new

--- app/loading.js
+++ app/loading.js
@@ -1,22 +1,25 @@
 "use client";
 import React from "react";
-import animationData from "../public/lottie/loadingLottie.json";
-import Lottie from "react-lottie";
-const Loading = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
+import Image from "next/image"; // Import Image from Next.js
 
+const Loading = () => {
   return (
     <div className="absolute flex justify-center items-center w-full h-full bg-slate-200">
-      <div>
-        {/* <Lottie options={defaultOptions} height={200} width={200} /> */}
-        <p>Loading...</p>
+      <div className="relative flex justify-center items-center">
+        {/* Logo in the center */}
+        <Image 
+          src="/logoSHARI.jpg" // Update with your logo path
+          alt="Logo"
+          width={100} // Adjust the width as needed
+          height={100} // Adjust the height as needed
+          className="z-10 w-24 h-24" // Ensures the logo appears above the spinner
+        />
+        
+        {/* Interactive Spinner surrounding the logo */}
+        <div className="absolute h-48 w-48 rounded-full border-8 border-t-transparent border-blue-500 animate-spin">
+          {/* Inner spinner for extra visual effect */}
+          <div className="h-full w-full rounded-full border-8 border-t-transparent border-green-400 animate-ping absolute top-0 left-0" />
+        </div>
       </div>
     </div>
   );
--- pages/frontend/ProductsPage.jsx
+++ pages/frontend/ProductsPage.jsx
@@ -119,98 +119,24 @@ const ProductsPage = () => {
 
   return (
     <div className="bg-bgmain2 md:pt-20">
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <div className="flex flex-col md:flex-row">
-          <div className="flex flex-col bg-white p-4 shadow-md md:hidden sticky top- z-10 rounded-lg">
-            <div className="flex items-center border border-gray-300 rounded-md mb-2">
-              <GiMagnifyingGlass className="h-5 w-5 text-gray-400 ml-2" />
-              <input
-                type="text"
-                value={searchQuery}
-                onChange={handleSearchChange}
-                placeholder="Search products..."
-                className="p-2 w-full outline-none rounded-md focus:ring-2 focus:ring-blue-500"
-              />
-            </div>
-
-            <div className="relative mb-2">
-              <select
-                value={selectedCategory}
-                onChange={(e) => handleCategoryChange(e.target.value)}
-                className="block w-full p-2 border border-gray-300 rounded-md appearance-none pr-8"
-              >
-                <option value="">All Categories</option>
-                {categories.map((category) => (
-                  <option key={category.id} value={category.id}>
-                    {category.category}
-                  </option>
-                ))}
-              </select>
-              <BsChevronBarDown className="absolute right-3 top-2.5 h-5 w-5 text-gray-400 pointer-events-none" />
-            </div>
-
-            <div className="relative">
-              <select
-                value={sortOrder}
-                onChange={handleSortChange}
-                className="block w-full p-2 border border-gray-300 rounded-md appearance-none pr-8"
-              >
-                <option value="asc">Sort by A-Z</option>
-                <option value="desc">Sort by Z-A</option>
-              </select>
-              <BsChevronBarDown className="absolute right-3 top-2.5 h-5 w-5 text-gray-400 pointer-events-none" />
-            </div>
-          </div>
-
-          {/* Product Grid */}
-          <div className="w-full md:w-3/4 ">
-            <h2 className="text-6xl font-bold  text-white px-5  pt-5  md:pl-10 md:pt-10 allura">
-              Products
-            </h2>
-            {filteredProducts.length > 0 ? (
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 px-5 py-5 md:px-20 md:py-5">
-                {filteredProducts.map((product) => (
-                  <Link key={product.id} href={`/products/${product.id}`}>
-                    <div className="bg-white rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105">
-                      <img
-                        src={product.imgURL}
-                        alt={product.title}
-                        className="w-full h-48 object-cover"
-                      />
-                      <div className="p-4">
-                        <h3 className="text-lg font-semibold text-gray-800">
-                          {product.title}
-                        </h3>
-                        <p className="text-sm text-gray-600">
-                          Rating: {product.rating}
-                        </p>
-                      </div>
-                    </div>
-                  </Link>
-                ))}
-              </div>
-            ) : (
-              <p>No products found.</p>
-            )}
-          </div>
-
-          {/* Right Side Filter Section for Desktop */}
-          <div className="hidden md:block w-1/4  bg-white shadow-lg mt-20 sticky top-0">
-            <h2 className="text-xl font-bold mb-4">Filters</h2>
+      <div className="flex flex-col md:flex-row">
+        <div className="flex flex-col bg-white p-4 shadow-md md:hidden sticky top-16 z-10 rounded-lg">
+          <div className="flex items-center border border-gray-300 rounded-md mb-2">
+            <GiMagnifyingGlass className="h-5 w-5 text-gray-400 ml-2" />
             <input
               type="text"
               value={searchQuery}
               onChange={handleSearchChange}
               placeholder="Search products..."
-              className="p-2 border border-gray-300 rounded-md mb-2"
+              className="p-2 w-full outline-none rounded-md focus:ring-2 focus:ring-blue-500"
             />
-            <h3 className="text-lg font-semibold mb-2">Categories</h3>
+          </div>
+
+          <div className="relative mb-2">
             <select
               value={selectedCategory}
               onChange={(e) => handleCategoryChange(e.target.value)}
-              className="p-2 border border-gray-300 rounded-md mb-4 w-full"
+              className="block w-full p-2 border border-gray-300 rounded-md appearance-none pr-8"
             >
               <option value="">All Categories</option>
               {categories.map((category) => (
@@ -219,36 +145,106 @@ const ProductsPage = () => {
                 </option>
               ))}
             </select>
+            <BsChevronBarDown className="absolute right-3 top-2.5 h-5 w-5 text-gray-400 pointer-events-none" />
+          </div>
 
-            <h3 className="text-lg font-semibold mb-2">Sort Options</h3>
+          <div className="relative">
             <select
               value={sortOrder}
               onChange={handleSortChange}
-              className="p-2 border border-gray-300 rounded-md w-full mb-4"
+              className="block w-full p-2 border border-gray-300 rounded-md appearance-none pr-8"
             >
               <option value="asc">Sort by A-Z</option>
               <option value="desc">Sort by Z-A</option>
             </select>
-
-            {/* Tags Filter */}
-            <h3 className="text-lg font-semibold mb-2">Tags</h3>
-            <ul className="space-y-2">
-              {tags.map((tag) => (
-                <li key={tag.id}>
-                  <label className="flex items-center space-x-2">
-                    <input
-                      type="checkbox"
-                      checked={selectedTags.includes(tag.id)}
-                      onChange={() => handleTagChange(tag.id)}
-                    />
-                    <span>{tag.tag}</span>
-                  </label>
-                </li>
-              ))}
-            </ul>
+            <BsChevronBarDown className="absolute right-3 top-2.5 h-5 w-5 text-gray-400 pointer-events-none" />
           </div>
         </div>
-      )}
+
+        {/* Product Grid */}
+        <div className="w-full md:w-3/4 ">
+          <h2 className="text-6xl font-bold  text-white px-5  pt-5  md:pl-10 md:pt-10 allura">
+            Products
+          </h2>
+          {filteredProducts.length > 0 ? (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 px-5 py-5 md:px-20 md:py-5">
+              {filteredProducts.map((product) => (
+                <Link key={product.id} href={`/products/${product.id}`}>
+                  <div className="bg-white rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105">
+                    <img
+                      src={product.imgURL}
+                      alt={product.title}
+                      className="w-full h-48 object-cover"
+                    />
+                    <div className="p-4">
+                      <h3 className="text-lg font-semibold text-gray-800">
+                        {product.title}
+                      </h3>
+                      <p className="text-sm text-gray-600">
+                        Rating: {product.rating}
+                      </p>
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          ) : (
+            <p>No products found.</p>
+          )}
+        </div>
+
+        {/* Right Side Filter Section for Desktop */}
+        <div className="hidden md:block w-1/4  bg-white shadow-lg mt-20 sticky top-0">
+          <h2 className="text-xl font-bold mb-4">Filters</h2>
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={handleSearchChange}
+            placeholder="Search products..."
+            className="p-2 border border-gray-300 rounded-md mb-2"
+          />
+          <h3 className="text-lg font-semibold mb-2">Categories</h3>
+          <select
+            value={selectedCategory}
+            onChange={(e) => handleCategoryChange(e.target.value)}
+            className="p-2 border border-gray-300 rounded-md mb-4 w-full"
+          >
+            <option value="">All Categories</option>
+            {categories.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.category}
+              </option>
+            ))}
+          </select>
+
+          <h3 className="text-lg font-semibold mb-2">Sort Options</h3>
+          <select
+            value={sortOrder}
+            onChange={handleSortChange}
+            className="p-2 border border-gray-300 rounded-md w-full mb-4"
+          >
+            <option value="asc">Sort by A-Z</option>
+            <option value="desc">Sort by Z-A</option>
+          </select>
+
+          {/* Tags Filter */}
+          <h3 className="text-lg font-semibold mb-2">Tags</h3>
+          <ul className="space-y-2">
+            {tags.map((tag) => (
+              <li key={tag.id}>
+                <label className="flex items-center space-x-2">
+                  <input
+                    type="checkbox"
+                    checked={selectedTags.includes(tag.id)}
+                    onChange={() => handleTagChange(tag.id)}
+                  />
+                  <span>{tag.tag}</span>
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
